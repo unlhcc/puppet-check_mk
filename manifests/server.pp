4b57ccc4548cc349de0dad2b19353946990528ee
Garhan Attebury <garhan.attebury@unl.edu>
2015-04-29 12:08:24 -0500

    bumped max timeout for check_mk exec to 1200 seconds

--- manifests/server.pp
+++ manifests/server.pp
@@ -10,6 +10,7 @@ define check_mk::server {
     # force a check_mk update every puppet run
     exec { "check_mk_update_${name}":
         command => "/bin/su - ${name} -c 'check_mk -I ; check_mk -O'",
+        timeout => 1200,
     }
 
 }
