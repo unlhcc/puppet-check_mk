e3c3bcd141c8d485f10f75dfa19f0c2351cb950c
Garhan Attebury <garhan.attebury@unl.edu>
2025-03-05 07:54:13 -0600

    EL9 and CheckMK 2.x support along with delint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,38 @@
-
+# Install and configure check-mk-agent
 class check_mk (
-    $omd_site        = $check_mk::params::omd_site,
-    $install         = $check_mk::params::install,
-    $check_mk_tags   = $check_mk::params::check_mk_tags,
-    $check_mk_alias  = $check_mk::params::check_mk_alias,
-    $check_mk_server = $check_mk::params::check_mk_server,
-    $plugin_mk_inventory_enable = $check_mk::params::plugin_mk_inventory_enable,
-    $plugin_mk_inventory_source = $check_mk::params::plugin_mk_inventory_source,
-    $plugin_active_location     = $check_mk::params::plugin_active_location,
+  $omd_site        = $check_mk::params::omd_site,
+  $install         = $check_mk::params::install,
+  $check_mk_tags   = $check_mk::params::check_mk_tags,
+  $check_mk_alias  = $check_mk::params::check_mk_alias,
+  $check_mk_server = $check_mk::params::check_mk_server,
+  $plugin_mk_inventory_enable = $check_mk::params::plugin_mk_inventory_enable,
+  $plugin_mk_inventory_source = $check_mk::params::plugin_mk_inventory_source,
+  $plugin_active_location     = $check_mk::params::plugin_active_location,
 ) inherits check_mk::params {
+  if $check_mk::install == 'agent' {
+    $omd_site_home = "/opt/omd/sites/${omd_site}"
+    $check_mk_location = "${omd_site_home}/etc/check_mk"
+    $check_mk_agent_location = "${check_mk_location}/agents"
 
-    if $check_mk::install == 'agent' {
+    case $facts['os']['family'] {
+      'RedHat': {
+        package { 'check-mk-agent': ensure => latest }
+      }
 
-        $omd_site_home = "/opt/omd/sites/${omd_site}"
-        $check_mk_location = "${omd_site_home}/etc/check_mk"
-        $check_mk_agent_location = "${check_mk_location}/agents"
-
-        case $::osfamily {
-            'RedHat': {
-                package { 'check-mk-agent': ensure => latest }
-            }
-
-            default: { notify { 'No support for check_mk agent on this OS': } }
-        }
-
-        check_mk::agent { $omd_site:
-            omd_site_home           => $omd_site_home,
-            check_mk_location       => $check_mk_location,
-            check_mk_agent_location => $check_mk_agent_location,
-            check_mk_server         => $check_mk_server,
-            check_mk_tags           => $check_mk_tags,
-            check_mk_alias          => $check_mk_alias,
-        }
+      default: { notify { 'No support for check_mk agent on this OS': } }
     }
 
-    if $check_mk::install == 'server' {
-        check_mk::server { $omd_site: }
+    check_mk::agent { $omd_site:
+      omd_site_home           => $omd_site_home,
+      check_mk_location       => $check_mk_location,
+      check_mk_agent_location => $check_mk_agent_location,
+      check_mk_server         => $check_mk_server,
+      check_mk_tags           => $check_mk_tags,
+      check_mk_alias          => $check_mk_alias,
     }
+  }
 
+  if $check_mk::install == 'server' {
+    check_mk::server { $omd_site: }
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,16 +1,22 @@
-
+# CheckMK Parameters
 class check_mk::params {
+  $omd_site = undef
+  $install = false
 
-    $omd_site = undef
-    $install = false
+  $check_mk_tags = undef
+  $check_mk_alias = undef
 
-    $check_mk_tags = undef
-    $check_mk_alias = undef
+  $check_mk_server = '127.0.0.1'
 
-    $check_mk_server = '127.0.0.1'
+  $plugin_mk_inventory_enable  = false
+  $plugin_mk_inventory_source  = '/usr/share/check-mk-agent/available-plugins/mk_inventory'
 
-    $plugin_mk_inventory_enable  = false
-    $plugin_mk_inventory_source  = '/usr/share/check-mk-agent/available-plugins/mk_inventory'
-    $plugin_active_location      = '/usr/share/check-mk-agent/plugins'
-
+  if (versioncmp($facts['os']['release']['major'], '8') <= 0) {
+    $plugin_active_location = '/usr/share/check-mk-agent/plugins'
+    $plugin_local_location = '/usr/share/check-mk-agent/local'
+  }
+  else {
+    $plugin_active_location = '/usr/lib/check_mk_agent/plugins'
+    $plugin_local_location = '/usr/lib/check_mk_agent/local'
+  }
 }
