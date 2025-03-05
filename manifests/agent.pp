a0e793fef6380ea76ec7f5259f1e5a3e70d81f26
Garhan Attebury <garhan.attebury@unl.edu>
2025-03-05 07:54:13 -0600

    EL9 and CheckMK 2.x support along with delint

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,26 +1,25 @@
-
+# CheckMK Agent
 define check_mk::agent (
+  $check_mk_tags   = $check_mk::params::check_mk_tags,
+  $check_mk_alias  = $check_mk::params::check_mk_alias,
+  $check_mk_server = $check_mk::params::check_mk_server,
 
-    $check_mk_tags   = $check_mk::params::check_mk_tags,
-    $check_mk_alias  = $check_mk::params::check_mk_alias,
-    $check_mk_server = $check_mk::params::check_mk_server,
+  $omd_site_home           = "/opt/omd/sites/${name}",
+  $check_mk_location       = "${omd_site_home}/etc/check_mk",
+  $check_mk_agent_location = "${check_mk_location}/agents",
 
-    $omd_site_home           = "/opt/omd/sites/${name}",
-    $check_mk_location       = "${omd_site_home}/etc/check_mk",
-    $check_mk_agent_location = "${check_mk_location}/agents",
-
-    $plugin_mk_inventory_enable  = $check_mk::params::plugin_mk_inventory_enable,
-    $plugin_mk_inventory_source  = $check_mk::params::plugin_mk_inventory_source,
-    $plugin_active_location      = $check_mk::params::plugin_active_location,
+  $plugin_mk_inventory_enable  = $check_mk::params::plugin_mk_inventory_enable,
+  $plugin_mk_inventory_source  = $check_mk::params::plugin_mk_inventory_source,
+  $plugin_local_location       = $check_mk::params::plugin_local_location,
+  $plugin_active_location      = $check_mk::params::plugin_active_location,
 
 ) {
-
-    # exported resource for a monitored host
-    @@file { "${check_mk_location}/conf.d/main/${::fqdn}.mk":
-            ensure  => file,
-            content => template('check_mk/main.erb'),
-            tag     => 'check_mk_remote',
-    }
+  # exported resource for a monitored host
+  @@file { "${check_mk_location}/conf.d/main/${facts['networking']['fqdn']}.mk":
+    ensure  => file,
+    content => template('check_mk/main.erb'),
+    tag     => 'check_mk_remote',
+  }
 
   # Only manage xinetd.d config on EL8 and prior
   if (versioncmp($facts['os']['release']['major'], '8') <= 0) {
@@ -35,38 +34,36 @@ define check_mk::agent (
     }
   }
 
-    # symlink mk_inventory plugin to activate if enabled
-    if $check_mk::plugin_mk_inventory_enable == true {
-        file { "${plugin_active_location}/mk_inventory":
-            ensure => 'link',
-            target => $check_mk::plugin_mk_inventory_source,
-        }
+  # symlink mk_inventory plugin to activate if enabled
+  if $check_mk::plugin_mk_inventory_enable == true {
+    file { "${plugin_active_location}/mk_inventory":
+      ensure => 'link',
+      target => $check_mk::plugin_mk_inventory_source,
     }
-    else
-    {
-        file { "${plugin_active_location}/mk_inventory":
-            ensure => 'absent',
-        }
+  }
+  else {
+    file { "${plugin_active_location}/mk_inventory":
+      ensure => 'absent',
     }
+  }
 
-    # copy local checks directory
-    file { '/usr/share/check-mk-agent/local':
-        ensure       => directory,
-        recurse      => remote,
-        mode         => '0755',
-        sourceselect => all,
-        source       => [
-            "puppet:///modules/check_mk/local_checks.${::facts[os][family]}-${::facts[os][release][major]}",
-            'puppet:///modules/check_mk/local_checks',
-        ],
-    }
+  # copy local checks directory
+  file { $plugin_local_location:
+    ensure       => directory,
+    recurse      => remote,
+    mode         => '0755',
+    sourceselect => all,
+    source       => [
+      "puppet:///modules/check_mk/local_checks.${::facts[os][family]}-${::facts[os][release][major]}",
+      'puppet:///modules/check_mk/local_checks',
+    ],
+  }
 
-    # copy plugins directory
-    file { $plugin_active_location:
-        ensure  => directory,
-        recurse => remote,
-        mode    => '0755',
-        source  => 'puppet:///modules/check_mk/plugins',
-    }
-
+  # copy plugins directory
+  file { $plugin_active_location:
+    ensure  => directory,
+    recurse => remote,
+    mode    => '0755',
+    source  => 'puppet:///modules/check_mk/plugins',
+  }
 }
--- manifests/server.pp
+++ manifests/server.pp
@@ -1,24 +1,22 @@
 
 define check_mk::server {
+  $omd_site_home           = "/opt/omd/sites/${name}"
+  $check_mk_location       = "${omd_site_home}/etc/check_mk"
+  $check_mk_agent_location = "${check_mk_location}/agents"
 
-    $omd_site_home           = "/opt/omd/sites/${name}"
-    $check_mk_location       = "${omd_site_home}/etc/check_mk"
-    $check_mk_agent_location = "${check_mk_location}/agents"
+  # realize the .mk files for all check_mk_remote resources
+  File<<| tag == 'check_mk_remote' |>>
 
-    # realize the .mk files for all check_mk_remote resources
-    File<<| tag == 'check_mk_remote' |>>
-
-    # FIXME: This was used to automagically inventory new services / reload OMD
-    # every puppet run, but 'check_mk -I' can get stuck on badly behaving hosts
-    # (tcp wait) and back up until everything explodes
-    #
-    # possibly use --no-tcp instead? still requires manual -I for new hosts...
-    # for now just commenting it out completely
-    #
-    # force a check_mk update every puppet run
-    #exec { "check_mk_update_${name}":
-    #    command => "/bin/su - ${name} -c 'check_mk -I ; check_mk -O'",
-    #    timeout => 1200,
-    #}
-
+  # FIXME: This was used to automagically inventory new services / reload OMD
+  # every puppet run, but 'check_mk -I' can get stuck on badly behaving hosts
+  # (tcp wait) and back up until everything explodes
+  #
+  # possibly use --no-tcp instead? still requires manual -I for new hosts...
+  # for now just commenting it out completely
+  #
+  # force a check_mk update every puppet run
+  #exec { "check_mk_update_${name}":
+  #    command => "/bin/su - ${name} -c 'check_mk -I ; check_mk -O'",
+  #    timeout => 1200,
+  #}
 }
